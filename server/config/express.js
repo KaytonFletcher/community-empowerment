5d5dd0336e424069d678e2d37dd0553023d8d467
Ank <a.vishnubhotla@ufl.edu>
2019-04-02 12:09:57 -0400

    express

--- server/config/express.js
+++ server/config/express.js
@@ -39,6 +39,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/cal', calRoutes);
 app.use('/api/auth', authRoutes);
 
+app.use('/', express.static(path.join(__dirname+'/node_modules')));
 
 app.all('/*', function(req, res) {
   res.sendFile(path.resolve('client/index.html'));
