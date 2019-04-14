8a9f292446f107417445207ff69d8b5a5c5b6077
Kayton <kfletcher1@ufl.edu>
2019-04-14 11:59:24 -0400

    Styled resources, can add videos via modal
    Can fill out form to add video, added title and tags to videos when viewing them.

--- client/js/controllers/videoController.js
+++ client/js/controllers/videoController.js
@@ -1,48 +1,66 @@
 angular.module('SpoderApp').controller('videoController', ['$scope', 'Video',
     function($scope, Video) {
+      $scope.videos = [];
 
       // $scope.event.userID = Authenticate.getUser(localStorage.getItem('token')); 
 
-        // Vids.getAll().then(function(res) {
-        //     $scope.videoReqs = res.data;
-        //     console.log('Video data displayed');
-        //   }, function(error) {
-        //     console.log('Unable to retrieve requests: ', error);
-        $scope.videos = [
-          {
-              "url": "https://www.youtube.com/embed/gGruZGY4Gvk"
+        Video.getAll().then(function(res) {
+            console.log(res.data);
+            $scope.videos = res.data;
 
-          },
-          {
-              "url": "https://www.youtube.com/embed/JGHWVAN_yQg"
-          }, 
-          {
-            "url": "https://www.youtube.com/embed/CzwGcARRVok"
-          }, 
-          {
-            "url": "https://www.youtube.com/embed/SYMi1QCzQEY"
-          }, 
-          {
-            "url": "https://www.youtube.com/embed/3dW-6iJSqQI"
-          }, 
-          {
-            "url": "https://www.youtube.com/embed/ZnWXtjQOxx0"
-          }
-        ];
+            console.log("IS IT AN ARRAY " + Array.isArray($scope.videos));
+
+        }, function(error) {
+            console.log('Unable to retrieve requests: ', error);
+          });
+
+      //   {
+      //     "url": "https://www.youtube.com/embed/gGruZGY4Gvk"
+
+      // },
+      // {
+      //     "url": "https://www.youtube.com/embed/JGHWVAN_yQg"
+      // }, 
+      // {
+      //   "url": "https://www.youtube.com/embed/CzwGcARRVok"
+      // }, 
+      // {
+      //   "url": "https://www.youtube.com/embed/SYMi1QCzQEY"
+      // }, 
+      // {
+      //   "url": "https://www.youtube.com/embed/3dW-6iJSqQI"
+      // }, 
+      // {
+      //   "url": "https://www.youtube.com/embed/ZnWXtjQOxx0"
+      // }
             
         $scope.deleteVideo = function(id) {
-            Video.delete(id).then(function(response){
+            Video.delete(id, localStorage.getItem('token')).then(function(response){
               for(var i = 0; i < $scope.videoReqs.length; i++){
                 if(response.data.title == $scope.videoReqs[i].title){
                   $scope.videoReqs.splice(i, 1);  
-                }}
+                }
+              }
               }, function(error) {
                 console.log('Unable to delete user: ', error);
               }
         )
       };
 
-        $scope.addVideo = function(id) {
+        $scope.addVideo = function() {
+          var url = $scope.newVideo.url.split('v=')[1];
+          if(url){
+            $scope.newVideo.url = 'https://www.youtube.com/embed/' + url.split('&')[0];
+          }
+
+          Video.add($scope.newVideo, localStorage.getItem('token')).then(function(res){  
+            if(res.data){
+              $scope.videos.push($scope.newVideo);
+            }else {console.log("error recieving video");}
+            $scope.newVideo = undefined;
+          }, function(err){
+              console.log(err);
+          });
           
         }
     }]);
