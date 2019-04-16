d976ce07bd5544cd3efcfd26ca205f1e9c7d53f1
Hannah Williams <hannahwilliams44@ufl.edu>
2019-04-16 15:41:04 -0400

    edit navbar

--- client/js/controllers/navBarController.js
+++ client/js/controllers/navBarController.js
@@ -2,8 +2,8 @@ angular.module('users').controller('NavbarCtrl', function ($scope, Authenticate)
 
     $scope.signoutPages = [
                     {'name': 'Home', 'ref': 'home'},
-                   // {'name': 'Resources', 'ref': 'entRes'},
-				   // {'name': 'Research', 'ref': 'research'},
+                    {'name': 'Resources', 'ref': 'entRes'},
+				    {'name': 'Research', 'ref': 'research'},
                     {'name': 'Calendar', 'ref': 'calendar'},
                     {'name': 'Affiliates and Partners', 'ref': 'affil'},
                 ];
