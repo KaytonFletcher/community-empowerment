8e85065696e25f5e3e7e0d2a664f66aaa0a5d3ff
Ank <a.vishnubhotla@ufl.edu>
2019-04-23 13:29:21 -0400

    Frontend factories updated

--- client/js/factories/videoFactory.js
+++ client/js/factories/videoFactory.js
@@ -2,14 +2,14 @@ angular.module('SpoderApp').factory('Video', function($http) {
     
     var methods = {
         getAll: function() {
-            return $http.get('http://localhost:8080/api/videos');
+            return $http.get('https://spoder.herokuapp.com/api/videos');
         },
         add: function(video, token) {
-            return $http.post('http://localhost:8080/api/videos/', video, { headers: {'x-access-token': token} });
+            return $http.post('https://spoder.herokuapp.com/api/videos/', video, { headers: {'x-access-token': token} });
         },
         delete: function(id, token) {
             //this is the post request for register, appending the user object to it (go to authRoutes.js)
-            return $http.delete('http://localhost:8080/api/videos/' + id, { headers: {'x-access-token': token} });
+            return $http.delete('https://spoder.herokuapp.com/api/videos/' + id, { headers: {'x-access-token': token} });
         }
     }
     
